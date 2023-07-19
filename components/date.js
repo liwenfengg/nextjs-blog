bb4fa1c7b3db311730581fcd3e7be8cb32a5b6c6
liwenfengg <ananas.leee@gmail.com>
2023-07-19 17:01:30 +0800

    feat: ts

--- components/date.js
+++
@@ -1,8 +0,0 @@
-import {parseISO, format} from 'date-fns'
-import {zhCN} from 'date-fns/locale'
-
-export default function Date({dateString}) {
-	const date = parseISO(dateString)
-
-	return <time dateTime={dateString}>{format(date, 'LLLL dd, yyyy', {locale: zhCN})}</time>
-}
--- components/layout.tsx
+++ components/layout.tsx
@@ -4,11 +4,15 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import { Analytics } from '@vercel/analytics/react';
+import React from 'react';
 
 const name = 'Daneel'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: {
+	children: React.ReactNode
+	home?: boolean
+}) {
 	return (
 		<div className={styles.container}>
 			<Head>
